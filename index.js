dd3b88193fe74e319707b02b803a3af4e6beb37e
mardIn smidty <147711727+Martin336699@users.noreply.github.com>
2025-01-22 14:44:14 +0100

    work is done

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  res.json({ ipaddress: '152.151.956',
+             language: 'denglisch',
+             software: 'linux'
+           });
 });
 
 // listen for requests :)
